34b4d8e74c44028ade5a1dbe6c7a13d7e61ad84f
Adhithya Rajasekaran <adhithyan15@gmail.com>
2021-06-13 16:30:02 -0700

    Adding a stopwatch implementation

--- languages/ruby/stopwatchy/lib/stopwatchy.rb
+++ languages/ruby/stopwatchy/lib/stopwatchy.rb
@@ -3,6 +3,53 @@
 require_relative "stopwatchy/version"
 
 module Stopwatchy
-  class Error < StandardError; end
-  # Your code goes here...
+  class StopWatchNeverStartedError < StandardError
+
+  end
+  class StopWatchNeverStoppedError < StandardError
+
+  end
+  class StopWatch
+    
+    def initialize(monotonically_increasing_clock = false)
+      @start_time_stamp = nil
+      @stop_time_stamp = nil
+      @monotonically_increasing_clock = monotonically_increasing_clock
+    end
+
+    def start()
+      if @monotonically_increasing_clock
+        @start_time_stamp = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+      else
+        @start_time_stamp = Time.now
+      end
+    end
+
+    def stop()
+      if @start_time_stamp.nil?
+        raise StopWatchNeverStartedError.new
+      end
+
+      if @monotonically_increasing_clock
+        @stop_time_stamp = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+      else
+        @stop_time_stamp = Time.now
+      end
+    end
+
+    def get_elapsed_time()
+      if @start_time_stamp.nil?
+        raise StopWatchNeverStartedError.new
+      end
+
+      if @stop_time_stamp.nil?
+        raise StopWatchNeverStoppedError.new
+      end
+
+      elapsed_time = @stop_time_stamp - @start_time_stamp
+      @stop_time_stamp = nil
+      @start_time_stamp = nil
+      return elapsed_time
+    end
+  end
 end
---
+++ languages/ruby/stopwatchy/test/stop_watch_test.rb
@@ -0,0 +1,41 @@
+# typed: ignore
+require_relative "../lib/Stopwatchy"
+require "test/unit"
+
+class StopWatchTest < Test::Unit::TestCase
+    def test_stop_watch_never_started()
+        assert_raise Stopwatchy::StopWatchNeverStartedError do
+            stopwatch = Stopwatchy::StopWatch.new
+            stopwatch.stop()
+        end
+    end
+
+    def test_get_elapsed_time_without_starting_stopwatch()
+        assert_raise Stopwatchy::StopWatchNeverStartedError do
+            stopwatch = Stopwatchy::StopWatch.new
+            stopwatch.get_elapsed_time()
+        end
+    end
+
+    def test_stop_watch_never_stopped()
+        assert_raise Stopwatchy::StopWatchNeverStoppedError do
+            stopwatch = Stopwatchy::StopWatch.new
+            stopwatch.start()
+            stopwatch.get_elapsed_time()
+        end
+    end
+
+    def test_get_elapsed_time()
+        stopwatch = Stopwatchy::StopWatch.new
+        stopwatch.start()
+        stopwatch.stop()
+        assert(stopwatch.get_elapsed_time() > 0, "Elapsed time must be greater than zero")
+    end
+
+    def test_get_elapsed_time_monotonically_increasing()
+        stopwatch = Stopwatchy::StopWatch.new(true)
+        stopwatch.start()
+        stopwatch.stop()
+        assert(stopwatch.get_elapsed_time() > 0, "Elapsed time must be greater than zero")
+    end
+end
