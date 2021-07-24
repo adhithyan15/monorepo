9b62e181667cfaad8dc5494b18a0ee18397f325c
Adhithya Rajasekaran <adhithyan15@gmail.com>
2021-07-24 14:21:07 -0700

    Use StopWatch To Time Operations

--- languages/javascript/operation-abstraction-browser/src/index.ts
+++ languages/javascript/operation-abstraction-browser/src/index.ts
@@ -1,9 +1,12 @@
 import { OperationFactory } from "../../operation/src/interfaces/OperationFactory";
 import { OperationFactoryImpl } from "../../operation/src/implementations/OperationFactoryImpl";
 import { StructuredLogger } from "../../structured-logger/src/interfaces/StructuredLogger";
+import { StopWatchFactory } from "../../stopwatchy/src/interfaces/StopWatchFactory";
+import { getStopWatchFactory } from "../../stopwatchy-browser/src/index";
 
 export function getOperationFactory(
   logger: StructuredLogger
 ): OperationFactory {
-  return new OperationFactoryImpl(logger);
+  const stopWatchFactory: StopWatchFactory = getStopWatchFactory(logger);
+  return new OperationFactoryImpl(stopWatchFactory, logger);
 }
--- languages/javascript/operation/src/implementations/OperationImpl.ts
+++ languages/javascript/operation/src/implementations/OperationImpl.ts
@@ -1,16 +1,28 @@
 import { Operation } from "../interfaces/Operation";
 import { StructuredLogger } from "../../../structured-logger/src/interfaces/StructuredLogger";
+import { StopWatch } from "../../../stopwatchy/src/interfaces/StopWatch";
+import { OperationLog } from "../interfaces/OperationLog";
 
 export class OperationImpl<OperationReturnType> implements Operation<OperationReturnType> {
   constructor(
     private operationName: string,
     private callbackFunction: () => OperationReturnType,
+    private stopWatch: StopWatch,
     private logger: StructuredLogger
   ) {}
   public getOperationName(): string {
     return this.operationName;
   }
   public getResult(): OperationReturnType {
-    return this.callbackFunction();
+    this.stopWatch.start();
+    const resultToReturn: OperationReturnType = this.callbackFunction();
+    this.stopWatch.stop();
+    const elapsedTime: number = this.stopWatch.getElapsedTime();
+    const operationLog: OperationLog = {
+      operationName: this.operationName,
+      elapsedTime: elapsedTime
+    };
+    this.logger.logInfo(JSON.stringify(operationLog));
+    return resultToReturn;
   }
 }
