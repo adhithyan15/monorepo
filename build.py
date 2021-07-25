7f15e84797f97731efcd93c990f28a0188a2125c
Adhithya Rajasekaran <adhithyan15@users.noreply.github.com>
2021-07-24 19:48:47 -0700

    Build script requires yarn. But yarn doesn't seem to be installed by default. So, fixing that.

--- build.py
+++ build.py
@@ -14,6 +14,13 @@ def osIndependentPathAppend(basePath, pathToAppend):
 
 def buildJavaScriptProjects():
     print("Starting to build javascript projects\n")
+    print("Installing Yarn\n")
+    try:
+        process = subprocess.Popen(['npm', 'install', '-g', 'yarn'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+        stdout, stderror = process.communicate()
+    except FileNotFoundError:
+        print("Unable to install Yarn. Failing the build\n")
+        sys.exit(2)
     project_contents = os.listdir(".")
     base_dir_path = os.getcwd()
     for project in project_contents:
