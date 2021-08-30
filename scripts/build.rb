526d8b6bf4aba7c9861878ef00d3a4ec91efdc82
Adhithya Rajasekaran <adhithyan15@users.noreply.github.com>
2021-08-29 18:28:13 -0700

    Attempting to debug vcvarsall

--- scripts/build.rb
+++ scripts/build.rb
@@ -74,7 +74,7 @@ if RUBY_PLATFORM =~ /w32/
         exit(false);
     end
     vswhere_path = `vswhere -products * -latest -prerelease -property installationPath`
-    puts "#{vswhere_path}\\VC\\Auxillary\\Build\\vcvarsall.bat x64"
+    puts "#{vswhere_path.chomp}\\VC\\Auxillary\\Build\\vcvarsall.bat x64"
     unless system("#{vswhere_path}\\VC\\Auxillary\\Build\\vcvarsall.bat x64")
         puts "Unable to configure visual studio using vcvarsall"
         exit(false)
