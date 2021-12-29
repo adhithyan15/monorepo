aeb0cefc1cccc909ac1c82e36a3618db4a779d32
Adhithya Rajasekaran <adhithyan15@users.noreply.github.com>
2021-12-28 19:36:47 -0800

    Fixing build issue

--- packages/ruby/file-extension-information/lib/file-extension-information.rb
+++ packages/ruby/file-extension-information/lib/file-extension-information.rb
@@ -1,5 +1,5 @@
 require_relative "file-extension-information/version"
-require_relative "../../shared-code/file-extension-information/get_file_extension_information"
+require_relative "../../shared-code/file-extension-information/retrieve_file_extension_information"
 
 module FileExtensionInformation
     def self.get_file_extension_information(file_extension)
