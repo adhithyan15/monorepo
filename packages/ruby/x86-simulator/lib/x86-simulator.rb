5cc2584ea64a69dea336b408ee6a00908954d954
Adhithya Rajasekaran <adhithyan15@users.noreply.github.com>
2021-12-30 20:00:11 -0800

    Add X86 Simulator Package (#115)
    * Add Spec For File Extension Information
    
    * Add X86 Simulator Package
    
    * Adding a spec

---
+++ packages/ruby/x86-simulator/lib/x86-simulator.rb
@@ -0,0 +1,5 @@
+require_relative "x86-simulator/version"
+
+module X86Simulator
+
+end
---
+++ packages/ruby/x86-simulator/lib/x86-simulator/version.rb
@@ -0,0 +1,7 @@
+# typed: strong
+# frozen_string_literal: true
+
+module X86Simulator
+    VERSION = "0.1.0"
+end
+  
